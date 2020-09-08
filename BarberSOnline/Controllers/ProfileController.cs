cc96e0dd4fc824d2db649484d394fc2f6f4d892a
szesin <szesin98@gmail.com>
2020-09-09 03:57:07 +0800

    InitialProfile

---
+++ BarberSOnline/Controllers/ProfileController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BarberSOnline.Areas.Identity.Data;
+using BarberSOnline.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberSOnline.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<BarberSOnlineUser> _userManager;
+        private readonly RoleManager<BarberSOnlineUser> _roleManager;
+        public ProfileController(UserManager<BarberSOnlineUser> userManager, RoleManager<BarberSOnlineUser> roleManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var userRolesViewModel = new List<ProfileModel>();
+            foreach (BarberSOnlineUser user in users)
+            {
+                var thisViewModel = new ProfileModel();
+                thisViewModel.UserId = user.Id;
+                thisViewModel.UserName = user.UserName;
+                thisViewModel.Email = user.Email;
+                thisViewModel.FirstName = user.FirstName;
+                thisViewModel.LastName = user.LastName;
+                thisViewModel.DateofBirth = user.DOB;
+                thisViewModel.Address = user.Address;
+                thisViewModel.Roles = await GetUserRoles(user);
+                userRolesViewModel.Add(thisViewModel);
+
+
+            }
+            return View(userRolesViewModel);
+        }
+        private async Task<List<string>> GetUserRoles(BarberSOnlineUser user)
+        {
+            return new List<string>(await _userManager.GetRolesAsync(user));
+        }
+    }
+}
---
+++ BarberSOnline/Models/ProfileModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarberSOnline.Models
+{
+    public class ProfileModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string Address { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
